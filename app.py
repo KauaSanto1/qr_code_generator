886a29114a70ae0b22f5048abcf2f54494136a49
Kauã Santos <89539085+KauaSanto1@users.noreply.github.com>
2024-10-09 18:01:35 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, send_file
+import qrcode
+import io
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate_qr():
+    data = request.form.get('data')
+    if not data:
+        return "No data provided", 400
+
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    img_io = io.BytesIO()
+    img.save(img_io, 'PNG')
+    img_io.seek(0)
+
+    return send_file(img_io, mimetype='image/png', as_attachment=True, download_name='qr_code.png')
+
+if __name__ == '__main__':
+    app.run(debug=True)
